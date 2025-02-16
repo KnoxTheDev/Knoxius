34605a27eabce17996b316ce33f86285d72a7baa
Knoxy <tuxltuxl001@gmail.com>
2025-02-16 20:46:38 +0530

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,30 @@
+--[[
+
+  _  __                _             _    _       _     
+ | |/ /               (_)           | |  | |     | |    
+ | ' / _ __   _____  ___ _   _ ___  | |__| |_   _| |__  
+ |  < | '_ \ / _ \ \/ / | | | / __| |  __  | | | | '_ \ 
+ | . \| | | | (_) >  <| | |_| \__ \ | |  | | |_| | |_) |
+ |_|\_\_| |_|\___/_/\_\_|\__,_|___/ |_|  |_|\__,_|_.__/ 
+
+]]
+
 local games = {
-    [{game.PlaceId}] = "https://raw.githubusercontent.com/KnoxTheDev/Knoxius/main/Universal/Script.lua",
+    -- Replace with the actual PlaceId and script URL
+    -- [111111111] = "https://raw.githubusercontent.com/KnoxTheDev/Knoxius/main/Games/Game1Script.lua",
 }
 
-for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
+local universalScript = "https://raw.githubusercontent.com/KnoxTheDev/Knoxius/main/Universal/Script.lua"
+local universal = true
+
+for placeId, url in pairs(games) do
+    if game.PlaceId == placeId then
         loadstring(game:HttpGet(url))()
+        universal = false
         break
     end
+end
+
+if universal == true then
+    loadstring(game:HttpGet(universalScript))()
 end
